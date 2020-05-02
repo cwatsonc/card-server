b8ed3206303acfa79e312e144e898c648c82e119
Glitch (hadron-aurora) <none>
2020-05-02 04:20:34 +0000

    🚍👨 Checkpoint ./public/style.css:15713545/4 ./server.js:15747413/4

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ function getCardById(cards,id){
 
 var express = require('express'); 
 var app = express();
-var server = app.listen(process.env.PORT || 300);
+var server = app.listen(proce it ss.env.PORT || 300);
 app.use(express.static('public'));
 console.log('server running')
 
